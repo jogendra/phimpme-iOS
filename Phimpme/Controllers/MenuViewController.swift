2321222952cf7205681dac514beeb05953e48acf
imjog <imjog24@gmail.com>
2018-03-21 02:03:50 +0530

    Menu VC Config

---
+++ Phimpme/Controllers/MenuViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MenuViewController.swift
+//  Phimpme
+//
+//  Created by JOGENDRA on 21/03/18.
+//  Copyright © 2018 Jogendra Singh. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func didTapBackButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+}
