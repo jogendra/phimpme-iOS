d22b66856fadba6170cdb894d226bbe7e8ffd5bf
imjog <imjog24@gmail.com>
2018-03-22 15:00:59 +0530

    Pod NMAnimatedTabBarItem added

--- Podfile
+++ Podfile
@@ -6,5 +6,6 @@ target 'Phimpme' do
   use_frameworks!
 
   # Pods for Phimpme
+pod 'NMAnimatedTabBarItem'
 
 end
---
+++ Pods/NMAnimatedTabBarItem/Classes/NMAnimationtype.swift
@@ -0,0 +1,18 @@
+//
+//  NMAnimationtype.swift
+//  NMAnimatedTabBarItem
+//
+//  Created by Balu Naik on 3/20/18.
+//
+
+import Foundation
+
+//MARK: NMAnimation type constants
+
+@objc public enum NMAnimationtype : Int {
+    case None
+    case Bounce
+    case Rotation
+    case Transition
+    case Frame
+}
