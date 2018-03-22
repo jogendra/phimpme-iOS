7bf9ed0c1bfdab69390ca257c571c79e1737c8e6
imjog <imjog24@gmail.com>
2018-03-22 15:02:33 +0530

    Initieted tababar vc

---
+++ Phimpme/Controllers/MainTabBarViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabBarViewController.swift
+//  Phimpme
+//
+//  Created by JOGENDRA on 22/03/18.
+//  Copyright © 2018 Jogendra Singh. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
